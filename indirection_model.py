016decc6a3d2cb2b51bb8db9580b2ec23d264c09
Chaning Blake Mullinax <cbm5d@>
2019-12-08 23:18:21 -0600

    updating training method

--- indirection_model.py
+++ indirection_model.py
@@ -30,12 +30,13 @@ N = 1024
 hrri = hrri(N) # Identity vector for general use
 args = LTM(N, normalized=True)
 args.lookup("agent*verb*patient*open*close*store*query")
+max_tasks = 100000
 
 # TODO: Get outputs from encoder and store them here
 # encodings = np.zeros((3,6))
 encodings = ["cat", "ate", "toy"]
 
-# -- Input Gates --
+ -- Input Gates --
 # Set up the input gate neural networks (one for each wm slot)
 ig = np.empty(3, dtype=object)
 for i in range(3):
@@ -64,33 +65,54 @@ for i in range(3):
 
 # -- Train --
 # Set the parameters
+block_size = 200
 batch_size = 1
 epochs = 10
 validation_split = 0.5
-
+accuracy = 0
+cur_task = 0
+cur_block_task = 0
 # Store HRRs for the role*open/close*store combo
 input_combo = np.array([hrri, hrri])
 input_combo[0] = args.lookup("agent*open*store")
 input_combo[1] = args.lookup("agent*close*store")
+# training loop
+while accuracy < 95 and cur_task < max_tasks:
 
-# Size: 3 -> # of ig og gates,
-#       2 -> hrr convolved with open or close, 
-ig_vals = np.empty((3,2), dtype=float)
-og_vals = np.empty((3,2), dtype=float)
+    if cur_task % 200 == 0:
+        block_tasks_correct = 0
 
-# Iterate through each of the words
-for i in range(3):
-    # Iterate through each of the IGs and OGs
-    for j in range(2):
-        ig_vals[i,j] = ig.predict(input_combo[k])
-        og_vals[i,j] = og.predict(input_combo[k]) 
-	# max of result trains the model from the previous
-	# time step
-	if i-1 > 0:
-		ig[i-1].fit(max(ig_vals[i,:]))
-		og[i-1].fit(max(og_vals[i,:]))
 
-# -- Query --
+    # Size: 3 -> # of ig og gates,
+    #       2 -> hrr convolved with open or close, 
+    ig_vals = np.empty((3,2), dtype=float)
+    og_vals = np.empty((3,2), dtype=float)
+    
+    # Iterate through each of the words
+    for i in range(3):
+        # Iterate through each of the IGs and OGs
+        for j in range(3):
+            for k in range(2):
+                ig_vals[j,k] = ig.predict(input_combo[k])
+                og_vals[j,k] = og.predict(input_combo[k]) 
+            # max of result trains the model from the previous
+        # time step
+        if i-1 > 0:
+            ig[i-1].fit(max(ig_vals[i,:]))
+            og[i-1].fit(max(og_vals[i,:]))
+
+    # -- Query --
+    query_role = np.random.choice(["agent", "verb", "patient"])
+    query_hrr = [args.lookup(query_role + "*query*open"),
+                 args.lookup(query_role + "*query*close")]
+    for i in range(3):
+        for j in range(2):
+            og_vals[i,j] = og[i].predict(query_hrr[j])
+
+    # train net
+
+    cur_task += 1
+
 
 
 # -- Test --
