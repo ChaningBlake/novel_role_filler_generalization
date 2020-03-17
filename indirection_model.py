26767cef081e736c9b320133afac103bfe6d625d
Blake Mullinax <cbm5d@mtmail.mtsu.edu>
2020-03-17 04:58:06 +0000

    fixed accuracy and error in training third time step

--- indirection_model.py
+++ indirection_model.py
@@ -5,7 +5,7 @@ November 2019
 Blake Mullinax
 Original code from Mike Jovanovich 2017
 https://github.com/mpjovanovich/indirection
-/outputgate_no_ac_nn.r
+Rscript outputgate_no_ac_nn.r 1 100000 3 10 3 C C D F F SG
 
 TODO: [Description]
 
@@ -80,9 +80,10 @@ epochs = 10
 accuracy = 0
 cur_task = 0
 cur_block_task = 0
+block_tasks_correct = 0
 
 # Store HRRs for the role*(open/close)*store combo
-input_combo = np.empty((3,2), dtype=object)
+input_combo = np.empty((4,2), dtype=object)
 roles = ["agent", "verb", "patient"]
 action = ["*open*", "*close*"]
 for i in range(3):
@@ -94,13 +95,9 @@ for i in range(3):
 # -----------------------
 system('clear')
 print("Beginning training....\n")
-while accuracy < 95 and cur_task < max_tasks:
+while accuracy < 95.0 and cur_task < max_tasks:
     # This is to choose a random sentence from the training
     sample = np.random.randint(0,200)
-    
-    if cur_task % 200 == 0:
-        block_tasks_correct = 0
-
 
     # Size: 4 -> # of time steps (3 words plus a query)
     #       3 -> # of ig og gates,
@@ -147,28 +144,95 @@ while accuracy < 95 and cur_task < max_tasks:
     # j -> open or close value
     for i in range(3):
         for j in range(2):
+            ig_vals[3,i,j] = ig[i].predict(np.array([query_hrr[j]]))
             og_vals[3,i,j] = og[i].predict(np.array([query_hrr[j]]))
+        max_val[3,i,:] = [np.argmax(ig_vals[3,i,:]), np.argmax(og_vals[3,i,:])]
         # train net
+        # train the third time step with the max output from the query step
+        ig[i].fit(np.array([args.lookup(input_combo[2, max_val[2,i,0]])]),
+                  np.array([ig_vals[3,i,max_val[3,i,0]]]),
+                  verbose=0)
+        og[i].fit(np.array([args.lookup(input_combo[2, max_val[2,i,1]])]),
+                  np.array([og_vals[3,i,max_val[3,i,1]]]),
+                  verbose=0)
         # if open and storing the correct thing in wm
         if (og_vals[3,i,0] > og_vals[3,i,1]) and (wm[i] ==
         encodings[sample][role_dict[query_role]]):
-                ig[i].fit(np.array([args.lookup(input_combo[role_dict[query_role],max_val[2,i,0]])]),
+                # args.lookup(input_combo[role_dict[query_role],max_val[2,i,0]])
+                ig[i].fit(np.array([query_hrr[max_val[3,i,0]]]),
                            np.array([success_reward]),
                            verbose=0)
-                og[i].fit(np.array([args.lookup(input_combo[role_dict[query_role],max_val[2,i,1]])]),
+                og[i].fit(np.array([query_hrr[max_val[3,i,1]]]),
                            np.array([success_reward]),
                            verbose=0)
                 block_tasks_correct += 1
         else:
-            ig[i].fit(np.array([args.lookup(input_combo[role_dict[query_role],max_val[2,i,0]])]),
+            # [args.lookup(input_combo[role_dict[query_role],max_val[2,i,0]])]
+            ig[i].fit(np.array([query_hrr[max_val[3,i,0]]]),
                        np.array([default_reward]),
                        verbose=0)
-            og[i].fit(np.array([args.lookup(input_combo[role_dict[query_role],max_val[2,i,1]])]),
+            og[i].fit(np.array([query_hrr[max_val[3,i,1]]]),
                        np.array([default_reward]),
                        verbose=0)
+            
     cur_task += 1
     if cur_task % 200 == 0:
         print()
         print("Tasks Complete:", cur_task)
-        print("Block Accuracy: %.2f"%((block_tasks_correct/200)*100))
+        print("Block Tasks Correct:", block_tasks_correct)
+        accuracy = (block_tasks_correct/200)*100
+        print("Block Accuracy: %.2f"%accuracy)
         print()
+        block_tasks_correct = 0
+
+
+# -----------------
+#   Testing loop
+# -----------------
+block_tasks_correct = 0
+encodings = np.loadtxt("SG-10-test.txt", dtype=object)
+for sentence in encodings:
+    # This is to choose a random sentence from the training
+    sample = np.random.randint(0,100)
+
+    # Size: 4 -> # of time steps (3 words plus a query)
+    #       3 -> # of ig og gates,
+    #       2 -> hrr convolved with open or close,
+    ig_vals = np.empty((4,3,2), dtype=float)
+    og_vals = np.empty((4,3,2), dtype=float)
+    # In this case, 2 -> the index of the max value of ig_vals (index 0) or og_vals
+    max_val = np.empty((4,3,2), dtype=int)
+
+    # --- First Three Time Steps ---
+    # i -> The current word (time step)
+    # j -> The current working memory slot
+    # k -> open or close value
+    for i in range(3):
+        for j in range(3):
+            for k in range(2):
+                ig_vals[i,j,k] = ig[j].predict(np.array([args.lookup(input_combo[i,k])]))
+                og_vals[i,j,k] = og[j].predict(np.array([args.lookup(input_combo[i,k])]))
+            max_val[i,j,:] = [np.argmax(ig_vals[i,j,:]), np.argmax(og_vals[i,j,:])]
+            # if the input gate is open, store encoding in wm
+            if max_val[i,j,0] == 0:
+                wm[i] = encodings[sample][i]
+
+    # -- Query --
+    query_role = np.random.choice(roles)
+    query_hrr = [args.lookup(query_role + "*query*open"),
+                 args.lookup(query_role + "*query*close")]
+    # for each og predict query*open and query*close
+    # i -> current working memory slot
+    # j -> open or close value
+    for i in range(3):
+        for j in range(2):
+            ig_vals[3,i,j] = ig[i].predict(np.array([query_hrr[j]]))
+            og_vals[3,i,j] = og[i].predict(np.array([query_hrr[j]]))
+        max_val[3,i,:] = [np.argmax(ig_vals[3,i,:]), np.argmax(og_vals[3,i,:])]
+        
+        # if open and storing the correct thing in wm
+        if (og_vals[3,i,0] > og_vals[3,i,1]) and (wm[i] ==
+        encodings[sample][role_dict[query_role]]):
+                # args.lookup(input_combo[role_dict[query_role],max_val[2,i,0]])
+                block_tasks_correct += 1
+print("Generalization Accuracy:", block_tasks_correct)   
