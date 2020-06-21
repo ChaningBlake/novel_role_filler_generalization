39a0025fa93496c2b266bd3932163acdd5f53172
Blake Mullinax <cbm5d@mtmail.mtsu.edu>
2020-06-21 14:53:34 +0000

    updates to nested-end-dec and test.py

--- nested-enc-dec.py
+++ nested-enc-dec.py
@@ -1,30 +1,121 @@
 #!/usr/bin/env python3
 
-import keras
 import sys
+if len(sys.argv) != 4:
+    print()
+    print("Usage: %s <Corpus> <TrainingSet> <TestingSet>"%(sys.argv[0]))
+    print()
+    sys.exit(1)
+    
+import keras
 import numpy as np
 from os import system
 import encode # My module
+import pickle, random, string
 
-# Import encoder and decoder model
+
+# Import inner decoder
 with open('encoder_len5.json', 'r') as encoder_file, open('decoder_len5.json', 'r') as decoder_file:
     encoder_json = encoder_file.read()
     decoder_json = decoder_file.read()
-encoder_model = keras.models.model_from_json(encoder_json)
-decoder_model = keras.models.model_from_json(decoder_json)
-encoder_model.load_weights("encoder_len5.h5")
-decoder_model.load_weights("decoder_len5.h5")
+inner_encoder = keras.models.model_from_json(encoder_json)
+inner_decoder = keras.models.model_from_json(decoder_json)
+inner_encoder.load_weights("encoder_len5.h5")
+inner_decoder.load_weights("decoder_len5.h5")
+
+# Prepare input
+corpus = np.loadtxt(sys.argv[1], dtype=object)
+
+mapping = {}
+for letter in string.ascii_lowercase[:10]:
+   mapping[letter] = encode.onehot(random.choice(corpus))
+
+x_train = []
+roles = np.loadtxt(sys.argv[2], dtype=object)
+for sentence in roles:
+    x_train.append(np.array([mapping[letter] for letter in sentence]))
+
+# Convert encodings into gestalt representations
+X = [inner_encoder.predict(word) for word in x_train]
+X = np.array(X)
 
 
-# Check result
+        
+
+
+
+# Construct Outer Encoder Decoder
+# ----------------------------------
+# Encoder Construction
+hidden_size = 50
+encoder_input = keras.layers.Input(shape=(None, X.shape[2]))
+encoder_hidden = keras.layers.LSTM(hidden_size, return_state=True)
+# Tie them together
+encoder_output, enc_state_h, enc_state_c = encoder_hidden(encoder_input)
+# Don't need the encoder outputs, just need the states.
+encoder_states = [enc_state_h, enc_state_c]
+
+## Decoder Construction
+decoder_input = keras.layers.Input(shape=(None, preY.shape[2]))
+decoder_hidden = keras.layers.LSTM(hidden_size, return_sequences=True, return_state=True)
+# Tie it together
+decoder_hidden_output, decoder_state_h, decoder_state_c = decoder_hidden(decoder_input,
+                                                                         initial_state=encoder_states)
+decoder_dense = keras.layers.Dense(postY.shape[2], activation='softmax')
+# Connect output to hidden
+decoder_output = decoder_dense(decoder_hidden_output)
+
+
+# Finally, tie everything into a model
+model = keras.Model([encoder_input, decoder_input], decoder_output)
+
+# Compile it...
+model.compile(loss = keras.losses.categorical_crossentropy,
+               optimizer=keras.optimizers.Adam(),
+               metrics=['accuracy'])
+
+# Train it
+batch_size = 100
+epochs = 400
+history = model.fit([X,preY], postY,
+                    batch_size=batch_size,
+                    epochs=epochs,
+                    verbose=1)
+print('Accuracy:', model.evaluate([X,preY],postY)[1]*100.0, '%')
+
+
+
+# Now remove the Teacher Forcing
+
+# Encoder
+encoder_model = keras.Model(encoder_input, encoder_states)
+
+# Decoder
+decoder_state_input_h = keras.layers.Input(shape=(hidden_size,))
+decoder_state_input_c = keras.layers.Input(shape=(hidden_size,))
+# inputs to hidden
+decoder_states_input = [decoder_state_input_h, decoder_state_input_c]
+decoder_hidden_output, decoder_state_h, decoder_state_c = decoder_hidden(decoder_input,
+                                                                         initial_state=decoder_states_input)
+decoder_states = [decoder_state_h, decoder_state_c]
+# hidden to outputs
+decoder_output = decoder_dense(decoder_hidden_output)
+decoder_model = keras.Model(
+    [decoder_input] + decoder_states_input,
+    [decoder_output] + decoder_states)
+
+
+'''
+# Get result from outer decoder
 context = pass
 token = np.array(encode.onehot("start"))
 token = token.reshape([1, 1, token.shape[0]])
 result = np.zeros([1,6,28])
 output_length = 5
 for x in range(output_length+1):
-    out,h,c = decoder_model.predict([token]+context)
+    out,h,c = inner_decoder.predict([token]+context)
     token = np.round(out)
     context = [h,c]
     result[0,x,:] = token
 decoded_word = encode.check(result[0])
+'''
--- test.py
+++ test.py
@@ -1,93 +1,144 @@
 #!/usr/bin/env python3
+
+import sys
+if len(sys.argv) != 4:
+    print()
+    print("Usage: %s <Corpus> <TrainingSet> <TestingSet>"%(sys.argv[0]))
+    print()
+    sys.exit(1)
+    
+import keras
+import numpy as np
+from os import system
+import encode # My module
+import pickle, random, string
+
+
+# Import inner decoder
+with open('encoder_len5.json', 'r') as encoder_file, open('decoder_len5.json', 'r') as decoder_file:
+    encoder_json = encoder_file.read()
+    decoder_json = decoder_file.read()
+inner_encoder = keras.models.model_from_json(encoder_json)
+inner_decoder = keras.models.model_from_json(decoder_json)
+inner_encoder.load_weights("encoder_len5.h5")
+inner_decoder.load_weights("decoder_len5.h5")
+
+# Prepare input
+corpus = np.loadtxt(sys.argv[1], dtype=object)
+
+mapping = {}
+for letter in string.ascii_lowercase[:10]:
+   mapping[letter] = encode.onehot(random.choice(corpus))
+
+# Match letter combinations with words e.g. cef -> John Ate Fish
+x_train = []
+roles = np.loadtxt(sys.argv[2], dtype=object)
+for sentence in roles:
+    x_train.append([mapping[letter] for letter in sentence])
+x_train = np.array(x_train)
+
+# Fill X with the gestalt representations for each word.
+X = []
+print(inner_encoder.predict(x_train[0:1,0,:]))
+
+'''
+for i in range(len(x_train)):
+    sentence = []
+    for j in range(3):
+        sentence.append(inner_encoder.predict(x_train[i:i+1,j,:]))
+    X.append(sentence)
+X = np.array(X)
+'''
+
+
+
+
+
+
+
+
+
+'''
+#!/usr/bin/env python3
 import keras
 import numpy as np
 import pickle
+import encode
 
-# Open data sets
-try:
-    with open("short_words", "rb") as pickledData:
-        corpus = pickle.load(pickledData)
-except:
-    print("Error: Could not open file %s"%(sys.argv[1]))
-    sys.exit(1)
-
-trainSize = 50000
-length = None
-input_length = 5
-output_length = 5
-max_length = 5    # longest word in the corpus
-
-# Generate one-hot encoding for alphabet
-# (including start and stop)
-alphabet = [i for i in range(28)]
-labels = keras.utils.to_categorical(alphabet, 28)
-
-# Create a Dictionary of the letters to one-hot encodings
-mapping = {}
-for i in range(labels.shape[0]-2):
-    mapping[ chr(ord('a')+i) ] = labels[i+1]
-# I know it's not necessary...but I really want the first encoding
-# to be start and the last one to be stop
-mapping["start"] = labels[0]
-mapping["stop"] = labels[27]
+hidden_size = 50
 
 
-# A little function to check the decoder at the end
-def check_decoder(result, mapping):
-    for letter in result:
-        for key in mapping:
-            if np.array_equal(mapping[key],letter):
-                print(key, end='')
-    print()
-    return
-
-'''
-# encode the training set
-x_train = corpus[:trainSize]
-x_train = [list(i) for i in x_train]
-X = np.empty([len(x_train), max_length, 28])
-
-for i, word in enumerate(x_train):
-    temp = [mapping[sym] for sym in word]
-    # fill end of word with stop tokens
-    if len(temp) < max_length:
-           for i in range(max_length - len(temp)):
-               temp.append(mapping['stop'])
-    X[i] = np.array(temp)
-
-# In this case, we want the output the same as the input
-# plus start and stop encodings
-Y = []
-for input_vector in X:
-    Y.append(np.vstack([mapping["start"], input_vector, mapping["stop"]]))
-Y = np.array(Y)
-
-# Prepare inputs for Decoder
-preY = Y[:, 0:-1, :]
-postY = Y[:,1: ,:]
-'''
-
-
-# encode the training set
-x_train = corpus[:trainSize]
-x_train = [list(i) for i in x_train]
 X = []
-for word in x_train:
-    X.append(np.array([mapping[sym] for sym in word]))
+for i in range(100):
+    X.append(np.round(np.random.rand(3,50)))
 X = np.array(X)
+start = np.zeros(50)
+start[0] = 1
+stop = np.zeros(50)
+stop[49] = 1
 
-# In this case, we want the output the same as the input
-# plus start and stop encodings
 Y = []
 for input_vector in X:
-    Y.append(np.vstack([mapping["start"], input_vector, mapping["stop"]]))
+    Y.append(np.vstack([start, input_vector, stop]))
 Y = np.array(Y)
 
-# Prepare inputs for Decoder
-preY = []
-postY = []
-for i, encoding in enumerate(Y):
-    preY.append(Y[i][ 0:-1 , :])
-    postY.append(Y[i][ 1: , :])
-preY = np.array(preY)
-postY = np.array(postY)
+preY = Y[:, 0:-1, :]
+postY = Y[:, 1:, :]
+
+# Encoder Construction
+encoder_input = keras.layers.Input(shape=(None, X.shape[2]))
+encoder_hidden = keras.layers.LSTM(hidden_size, return_state=True)
+# Tie them together
+encoder_output, enc_state_h, enc_state_c = encoder_hidden(encoder_input)
+# Don't need the encoder outputs, just need the states.
+encoder_states = [enc_state_h, enc_state_c]
+
+## Decoder Construction
+decoder_input = keras.layers.Input(shape=(None, preY.shape[2]))
+decoder_hidden = keras.layers.LSTM(hidden_size, return_sequences=True, return_state=True)
+# Tie it together
+decoder_hidden_output, decoder_state_h, decoder_state_c = decoder_hidden(decoder_input,
+                                                                         initial_state=encoder_states)
+decoder_dense = keras.layers.Dense(postY.shape[2], activation='softmax')
+# Connect output to hidden
+decoder_output = decoder_dense(decoder_hidden_output)
+
+
+# Finally, tie everything into a model
+model = keras.Model([encoder_input, decoder_input], decoder_output)
+
+# Compile it...
+model.compile(loss = keras.losses.categorical_crossentropy,
+               optimizer=keras.optimizers.Adam(),
+               metrics=['accuracy'])
+
+# Train it
+batch_size = 100
+epochs = 400
+history = model.fit([X,preY], postY,
+                    batch_size=batch_size,
+                    epochs=epochs,
+                    verbose=1)
+print('Accuracy:', model.evaluate([X,preY],postY)[1]*100.0, '%')
+
+
+
+# Now remove the Teacher Forcing
+
+# Encoder
+encoder_model = keras.Model(encoder_input, encoder_states)
+
+# Decoder
+decoder_state_input_h = keras.layers.Input(shape=(hidden_size,))
+decoder_state_input_c = keras.layers.Input(shape=(hidden_size,))
+# inputs to hidden
+decoder_states_input = [decoder_state_input_h, decoder_state_input_c]
+decoder_hidden_output, decoder_state_h, decoder_state_c = decoder_hidden(decoder_input,
+                                                                         initial_state=decoder_states_input)
+decoder_states = [decoder_state_h, decoder_state_c]
+# hidden to outputs
+decoder_output = decoder_dense(decoder_hidden_output)
+decoder_model = keras.Model(
+    [decoder_input] + decoder_states_input,
+    [decoder_output] + decoder_states)
+'''
