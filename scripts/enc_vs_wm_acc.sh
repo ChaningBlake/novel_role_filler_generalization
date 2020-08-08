ab69d67abe28dd347be7991376c6b95d95f5b8b6
Blake Mullinax <cbm5d@mtmail.mtsu.edu>
2020-08-08 03:45:47 +0000

    routine cleaning

--- scripts/enc_vs_wm_acc.sh
+++ scripts/enc_vs_wm_acc.sh
@@ -1,8 +1,12 @@
-for i in SG SA FC NF
+for i in NF 
 do
-    for ((j=0; j<10; j++))
+    for ((j=0; j<5; j++))
     do
         #echo "./indirection_model_w_enc-dec.py data/$i-10-train.txt data/$i-10-test.txt | tail -2 >> *_wm_acc_${i}_2.txt"
-        echo "./nested-enc-dec.py data/len5_10000-train.txt data/$i-10-train.txt data/$i-10-test.txt | tail -3 >> *_nested_acc_${i}_2.txt"
+        #echo "./nested-enc-dec.py data/len5_10000-train.txt data/$i-10-train.txt data/$i-10-test.txt | tail -3 >> *_nested_acc_${i}_2.txt"
+        #echo "./e2e_enc_dec.py data/len5_10000-train.txt data/$i-10-train.txt data/$i-10-test.txt | tail -2 >> *_e2e_enc_${i}.txt"
+        #echo "./e2e_enc_dec_query.py data/len5_10000-train.txt data/$i-10-train.txt data/$i-10-test.txt 1 | tail -2 >> \*_e2e_enc_query_1_${i}.txt"
+        #echo "./e2e_enc_dec_query.py data/len5_10000-train.txt data/$i-10-train.txt data/$i-10-test.txt 5 | tail -2 >> \*_e2e_enc_query_5_${i}.txt"
+        echo "./e2e_enc_dec_query.py data/len5_10000-train.txt data/$i-10-train.txt data/$i-10-test.txt 10 | tail -2 >> \*_e2e_enc_query_10_${i}.txt"
     done
 done
