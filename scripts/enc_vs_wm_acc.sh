59d68d1c974c071f08287adcd8f76ebc9ac1d7f8
Blake Mullinax <cbm5d@mtmail.mtsu.edu>
2020-08-31 16:35:47 +0000

    adding all the superfluous files for moving to new server

--- scripts/enc_vs_wm_acc.sh
+++ scripts/enc_vs_wm_acc.sh
@@ -4,9 +4,13 @@ do
     do
         #echo "./indirection_model_w_enc-dec.py data/$i-10-train.txt data/$i-10-test.txt | tail -2 >> *_wm_acc_${i}_2.txt"
         #echo "./nested-enc-dec.py data/len5_10000-train.txt data/$i-10-train.txt data/$i-10-test.txt | tail -3 >> *_nested_acc_${i}_2.txt"
-        echo "./e2e_enc_dec.py data/len5_10000-train.txt data/$i-10-train.txt data/$i-10-test.txt | tail -2 >> \*_e2e_enc_${i}.txt"
-        echo "./e2e_enc_dec_query.py data/len5_10000-train.txt data/$i-10-train.txt data/$i-10-test.txt 1 | tail -2 >> \*_e2e_enc_query_1_${i}.txt"
-        echo "./e2e_enc_dec_query.py data/len5_10000-train.txt data/$i-10-train.txt data/$i-10-test.txt 5 | tail -2 >> \*_e2e_enc_query_5_${i}.txt"
-        echo "./e2e_enc_dec_query.py data/len5_10000-train.txt data/$i-10-train.txt data/$i-10-test.txt 10 | tail -2 >> \*_e2e_enc_query_10_${i}.txt"
+        #echo "./e2e_enc_dec.py data/len5_10000-train.txt data/$i-10-train.txt data/$i-10-test.txt | tail -2 >> \*_e2e_enc_${i}.txt"
+        #echo "./e2e_enc_dec_query.py data/len5_10000-train.txt data/$i-10-train.txt data/$i-10-test.txt 1 | tail -2 >> \*_e2e_enc_query_1_${i}.txt"
+        #echo "./e2e_enc_dec_query.py data/len5_10000-train.txt data/$i-10-train.txt data/$i-10-test.txt 5 | tail -2 >> \*_e2e_enc_query_5_${i}.txt"
+        #echo "./e2e_enc_dec_query.py data/len5_10000-train.txt data/$i-10-train.txt data/$i-10-test.txt 10 | tail -2 >> \*_e2e_enc_query_10_${i}.txt"
+        echo "./e2e_enc_dec_query.py data/len5_10000-train.txt data/$i-10-train.txt data/$i-10-test.txt 1 >> .${i}_${j}_output.txt"
     done
 done
+
+ #cat .${i}_*_output.txt > \*_e2e_enc_given_roles_${i}.txt
+ #rm .${i}_*_output.txt
